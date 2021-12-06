cadbc37f7123e0464c2ceb46cb3f7ac9e42c0c2e
suban-khoja-777 <69012061+suban-khoja-777@users.noreply.github.com>
2021-12-06 19:03:42 +0530

    Update RoundsDetailScreen.svelte

--- src/RoundsDetailScreen.svelte
+++ src/RoundsDetailScreen.svelte
@@ -1,19 +1,26 @@
 <script>
-
+import Icon from "./utility/Icon.svelte";
 import Button from "./utility/Button.svelte";
 
 export let goToGameScreen;
 export let rounds = [];
 export let openRoundDetail;
+export let totalRounds = 0;
 </script>
 
 <main>
     <main class="row">
         <h2> Round Details </h2>
+        <Icon text={rounds.length} />
+        {#if totalRounds}
+            <main>/</main>
+            <Icon text={totalRounds} />    
+        {/if}
+        
     </main>
 
     <main class="row">
-        <ul>
+        <ul class="round-list-container">
             {#each rounds as round , index}        
                 <Button text="Round {index+1}" type="primary" onClick={ () => openRoundDetail(round)} />
                 <br/>
