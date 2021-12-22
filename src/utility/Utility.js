05cdf0da0b8b669ac37f5f1795434b3bf4411ad4
suban-khoja-777 <69012061+suban-khoja-777@users.noreply.github.com>
2021-12-22 20:34:47 +0530

    Update Utility.js

--- src/utility/Utility.js
+++ src/utility/Utility.js
@@ -14,3 +14,21 @@ export const generateGameId = () => {
     return 'G-'+generateRandomId();
 }
 
+export const generateRoundId = () => {
+    return 'R-'+generateRandomId();
+}
+
+const getDateString = () => {
+    let dateArr = new Date().toDateString().split(' ');
+    dateArr.shift();
+    return dateArr[1] + ' ' + dateArr[0] + ' ' +dateArr[2];
+}
+
+const getTimeString = () => {
+    return new Date().toLocaleTimeString();
+}
+
+export const getDateTimeAsString = () => {
+    return getDateString() + ' ' + getTimeString();
+}
+
