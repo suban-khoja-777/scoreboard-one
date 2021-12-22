266f2f49549c0c0702ed38635b07b24215a68d8d
suban-khoja-777 <69012061+suban-khoja-777@users.noreply.github.com>
2021-12-22 20:35:09 +0530

    Add files via upload

---
+++ src/PreviousGamesLists.svelte
@@ -0,0 +1,31 @@
+<script>
+import Button from "./utility/Button.svelte";
+
+export let games = [];
+export let openGameDetail;
+export let goToHomeScreen;
+</script>
+
+<main>
+    <main class="row">
+        <h2> Previous Games </h2>
+    </main>
+
+    <main class="row">
+        <ul class="round-list-container">
+            {#each games as game}        
+                <Button text="Played on {game.date_time}" type="primary" onClick={ () => openGameDetail(game)} />
+                <br/>
+            {/each}
+        </ul>
+    </main>
+    
+    <main class="footer">
+        <Button text="Previous" type="secondary" onClick={goToHomeScreen} />
+    </main>
+    
+</main>
+
+<style>
+
+</style>
